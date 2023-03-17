0402eedd53415b0beedf38ec1069b4e706ad2b66
Olena Zamyslova <olenazamyslova@gmail.com>
2023-03-17 14:15:36 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      console.log(1),
     ],
     filter: '',
   };
