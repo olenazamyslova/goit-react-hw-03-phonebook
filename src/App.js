965b83064f4f9abb6fca5a008dc321802d2fe747
Olena Zamyslova <olenazamyslova@gmail.com>
2023-03-17 14:17:53 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,34 +12,46 @@ class App extends Component {
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      console.log(1),
     ],
     filter: '',
   };
+
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts) {
+      this.setState({ contacts: JSON.parse(savedContacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   formSubmitHandler = data => {
     this.repeatControl(data);
   };
 
   repeatControl = (data) => {
-  const { contacts } = this.state;
-  const nameArray = contacts.map((contact) => contact.name);
+    const { contacts } = this.state;
+    const nameArray = contacts.map((contact) => contact.name);
 
-  if (!nameArray.includes(data.name)) {
-    const newContact = {
-      id: uuidv4(),
-      name: data.name,
-      number: data.number,
-    };
+    if (!nameArray.includes(data.name)) {
+      const newContact = {
+        id: uuidv4(),
+        name: data.name,
+        number: data.number,
+      };
 
-    this.setState((prevState) => ({
-      ...prevState,
-      contacts: [...prevState.contacts, newContact],
-    }));
-  } else {
-    alert("Контакт вже є у телефонній книзі!!!");
-  }
-};
+      this.setState((prevState) => ({
+        ...prevState,
+        contacts: [...prevState.contacts, newContact],
+      }));
+    } else {
+      alert("Контакт вже є у телефонній книзі!!!");
+    }
+  };
 
   elementDelete = (arr, idContact) => {
     let newArr = arr.filter(elem => elem.id !== idContact);
@@ -47,34 +59,35 @@ class App extends Component {
   };
 
   deleteContactFromContactList = (idContact) => {
-  this.setState((prevState) => {
-    const updatedContacts = this.elementDelete(prevState.contacts, idContact);
-    return { contacts: updatedContacts };
-  });
-};
+    this.setState((prevState) => {
+      const updatedContacts = this.elementDelete(prevState.contacts, idContact);
+      return { contacts: updatedContacts };
+    });
+  };
 
   setFilterToState = filterData => {
     this.setState({ filter: `${filterData}` });
   };
 
   filterArr = () => {
-  return this.state.contacts.filter((cur) =>
-    cur.name.toUpperCase().includes(this.state.filter)
-  );
-};
+    return this.state.contacts.filter((cur) =>
+      cur.name.toUpperCase().includes(this.state.filter)
+    );
+  };
 
-render() {
-  const filteredContacts = this.filterArr();
+  render() {
+    const filteredContacts = this.filterArr();
 
-  return (
-    <div className="App">
-      <h1>Phonebook</h1>
-      <ContactForm onSubmitData={this.formSubmitHandler} />
-      <h1>Contacts</h1>
-      <Filter setFilterToState={this.setFilterToState} />
-      <ContactList contacts={filteredContacts} del={this.deleteContactFromContactList} />
-    </div>
-  );
+    return (
+      <div className="App">
+        <h1>Phonebook</h1>
+        <ContactForm onSubmitData={this.formSubmitHandler} />
+        <h1>Contacts</h1>
+        <Filter setFilterToState={this.setFilterToState} />
+        <ContactList contacts={filteredContacts} del={this.deleteContactFromContactList} />
+      </div>
+    );
+  }
 }
-}
+
 export default App;
